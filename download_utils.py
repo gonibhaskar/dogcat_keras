69bab97fa9ca0d6ebf12bfc953ad6f3a8704dde9
bhaskarbhardwaj007 <52116851+bhaskarbhardwaj007@users.noreply.github.com>
2020-05-25 12:21:36 -0700

    py

--- download_utils.py
+++ download_utils.py
@@ -18,7 +18,7 @@ def error_handle():
     exc_type, exc_obj, exc_tb = sys.exc_info()
     fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
     print(exc_type, fname, exc_tb.tb_lineno)
-
+    print("Hello")
 
 def retri_file_size(url: str):
     """Return to-be-downloaded file size
